02e9d599f1f6d6d8f07d9fe7b92c021a8d1bb965
HJtime <hj_time@naver.com>
2021-01-18 21:23:41 +0900

    add login component

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,10 +1,11 @@
 import firebase from 'firebase';
+import firebaseApp from './firebase';
 
 class AuthService{ //로그인 로그아웃 관련된 역할만 수행
     login(providerName){ //구글인지 트위터인지 providerName로 받아옴(반복되는 로직)
         const authProvider=new firebase.auth[`${providerName}AuthProvider`]();
 
-        return firebase.auth().signInWithPopup(authProvider);
+        return firebaseApp.auth().signInWithPopup(authProvider);
     }
 }
 
