f40b87bf75e3f1a51f4909aaa335cae326db337d
HJtime <hj_time@naver.com>
2021-01-19 20:02:50 +0900

    add card add form component

--- src/componentes/maker/maker.jsx
+++ src/componentes/maker/maker.jsx
@@ -50,13 +50,18 @@ const Maker = ({authService}) => {
                 history.push('/');
             }
         })
-    })
+    });
+
+    const addCrad=(card)=>{
+        const updated=[...cards, card];
+        setCards(updated);
+    }
 
     return(
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Editor cards={cards}/>
+                <Editor cards={cards} addCard={addCrad}/>
                 <Preview cards={cards}/>
             </div>
             <Footer/>
