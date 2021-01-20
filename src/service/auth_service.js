329bdbeb37247995e160bb598c86bd451350dd0b
HJtime <hj_time@naver.com>
2021-01-20 20:29:20 +0900

    remove warnings firebase clean up

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,21 +1,30 @@
-import firebase from 'firebase';
-import firebaseApp from './firebase';
+import {firebaseAuth, githubProvider, googleProvider} from './firebase';
 
-class AuthService{ //로그인 로그아웃 관련된 역할만 수행
-    login(providerName){ //구글인지 트위터인지 providerName로 받아옴(반복되는 로직)
-        const authProvider=new firebase.auth[`${providerName}AuthProvider`]();
-
-        return firebaseApp.auth().signInWithPopup(authProvider);
+class AuthService{
+    login(providerName){
+        const authProvider=this.getProvider(providerName);
+        return firebaseAuth.signInWithPopup(authProvider);
     }
 
     logout(){
-        firebase.auth().signOut();
+        firebaseAuth.signOut();
     }
 
     onAuthChange(onUserChanged){
-        firebase.auth().onAuthStateChanged(user=>{
+        firebaseAuth.onAuthStateChanged(user=>{
             onUserChanged(user);
         })
+    }
+
+    getProvider(providerName){
+        switch(providerName){
+            case 'Google':
+                return googleProvider;
+            case 'Github':
+                return githubProvider;
+                default:
+                throw new Error(`not supported provider ${providerName}`);
+        }
     }
 }
 
